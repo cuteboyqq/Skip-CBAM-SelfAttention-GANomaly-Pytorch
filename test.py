4299d89e945c5e73fa81ba1d88164b25582d02fd
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2023-01-13 13:27:42 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 27 13:51:45 2022
+@author: User
+"""
+import matplotlib as mpl
+mpl.rcParams.update(mpl.rcParamsDefault)
+import matplotlib.pyplot as plt
+import torch
+import torchvision.transforms as transforms
+import torchvision
+import torch.nn as nn
+from network import network
+from util import loss
+from util import plot
+import warnings
+from network.model import Ganomaly
+from network.umodel import UGanomaly
+from network.SAmodel import SAGanomaly
+from torch.serialization import SourceChangeWarning
+warnings.filterwarnings("ignore", category=SourceChangeWarning)
+
+def get_args():
+    import argparse
+    
+    parser = argparse.ArgumentParser()
+    #'/home/ali/datasets/train_video/NewYork_train/train/images'
+    parser.add_argument('-noramldir','--normal-dir',help='image dir',default=r"/home/ali/datasets/factory_data/2022-12-30-4cls-cropimg/crops_line")
+    parser.add_argument('-abnoramldir','--abnormal-dir',help='image dir',default= r"/home/ali/datasets/factory_data/2022-12-30-4cls-cropimg/crops_noline")
+    parser.add_argument('-imgsize','--img-size',type=int,help='image size',default=32)
+    parser.add_argument('-nz','--nz',type=int,help='compress size',default=100)
+    parser.add_argument('-nc','--nc',type=int,help='num of channels',default=3)
+    parser.add_argument('-lr','--lr',type=float,help='learning rate',default=2e-4)
+    parser.add_argument('-batchsize','--batch-size',type=int,help='train batch size',default=1)
+    parser.add_argument('-savedir','--save-dir',help='save model dir',default=r"/home/ali/GitHub_Code/cuteboyqq/GANomaly/skip-GANOMALY-Pytorch/runs/train/2023-01-06/32-nz100-ngf64-ndf64-SkipAttentionGanomaly")
+    parser.add_argument('-weights','--weights',help='model dir',default= r"/home/ali/GitHub_Code/cuteboyqq/GANomaly/skip-GANOMALY-Pytorch/runs/train/2023-01-06/32-nz100-ngf64-ndf64-SkipAttentionGanomaly")
+    parser.add_argument('-viewimg','--view-img',action='store_true',help='view images')
+    parser.add_argument('-train','--train',action='store_true',help='view images')
+    return parser.parse_args()    
+
+
+def main():
+    args = get_args()
+    
+    test(args)
+
+def test(args):
+    args.view_img = False
+    if args.view_img:
+        BATCH_SIZE_VAL = 1
+        SHOW_MAX_NUM = 10
+        shuffle = True
+    else:
+        BATCH_SIZE_VAL = 1
+        SHOW_MAX_NUM = 14400
+        shuffle = False
+    # convert data to torch.FloatTensor
+   
+    
+    test_loader = data_loader(shuffle,args.normal_dir,args)
+ 
+    defeat_loader = data_loader(shuffle,args.abnormal_dir,args)
+    # specify loss function
+    criterion = nn.MSELoss()
+    show_num = 0
+    normal_loss, anomaly_loss = [],[]
+    print('Start test :') 
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    #device = torch.device('cpu')
+    #model.eval()
+    #model = ConvAutoencoder()
+    #skip_ganomaly = UGanomaly(args)
+    #ganomaly = Ganomaly(args)
+    skip_attention_ganomaly = SAGanomaly(args)
+    #model = network.NetG(isize=IMAGE_SIZE_H, nc=3, nz=100, ngf=64, ndf=64, ngpu=1, extralayers=0)
+    #model = torch.load(modelPath).to(device)
+    #model.load_state_dict(torch.load(modelPath))
+    print('load model weight from {} success'.format(args.weights))
+    print('VAL_DATA_DIR : {}'.format(args.normal_dir))
+    
+    normal_loss = infer(test_loader,SHOW_MAX_NUM,skip_attention_ganomaly,criterion,normal_loss,
+            'normal',device,args)
+    
+    anomaly_loss = infer(defeat_loader,SHOW_MAX_NUM,skip_attention_ganomaly,criterion,anomaly_loss,
+            'anomaly',device,args)
+    
+    
+    if not args.view_img:
+        loss_list = [0.0,0.5,1.0,1.4,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.50,2.75,3.0,4.0,5.0,6.0]
+        skip_attention_ganomaly.plot_two_loss_histogram(normal_loss,anomaly_loss,"2023-01-03-ganomaly-histogram")
+        plot.plot_loss_distribution(SHOW_MAX_NUM,normal_loss,anomaly_loss)
+        
+        skip_attention_ganomaly.Analysis_two_list_UserDefineLossTH(normal_loss,anomaly_loss,"2023-01-03-ganomaly-histogram2",loss_list)
+class UnNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+            # The normalize code -> t.sub_(m).div_(s)
+        return tensor
+
+
+def compute_loss(outputs,images,criterion):
+    gen_imag, latent_i, latent_o = outputs
+    loss_con = loss.l2_loss(images, gen_imag)
+    loss_enc = loss.l1_loss(latent_i, latent_o)
+    loss_sum = loss_enc + 50*loss_con
+    return loss_sum
+
+
+def renormalize(tensor):
+        minFrom= tensor.min()
+        maxFrom= tensor.max()
+        minTo = 0
+        maxTo=1
+        return minTo + (maxTo - minTo) * ((tensor - minFrom) / (maxFrom - minFrom))
+
+def infer(data_loader,
+          SHOW_MAX_NUM,
+          model,
+          criterion,
+          loss_list,
+          data_type,
+          device,
+          args
+          ):
+    show_num = 0
+    model.eval()
+    #with torch.no_grad():
+    
+    dataiter = iter(data_loader)
+    cnt=1
+    
+    while(show_num < SHOW_MAX_NUM):
+        images, labels = dataiter.next()
+        print('{} Start {} AE:'.format(show_num,data_type))
+        # get sample outputs
+        images = images.to(device)
+        outputs = model(images)
+        #gen_imag, latent_i, latent_o = outputs
+        error_g, error_d, fake_img, model_g, model_d = outputs
+        loss = error_g
+        #loss = compute_loss(outputs,images,criterion)
+        loss_list.append(float(loss.detach().cpu().numpy()))
+        print('loss : {}'.format(loss))
+        
+        #unorm = UnNormalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+        #images = unorm(images)
+        #fake_img = unorm(fake_img.data)
+        images = renormalize(images)
+        fake_img = renormalize(fake_img)
+        
+        
+        
+        #images = images.view(args.batch_size, 3, args.img_size, args.img_size)
+        images = images.cpu().numpy()
+        fake_img = fake_img.view(args.batch_size, 3, args.img_size, args.img_size)
+        fake_img = fake_img.cpu().detach().numpy()
+        
+       
+        if args.view_img:
+            import os
+            os.makedirs('./runs/detect',exist_ok=True)
+            plts = plot.plot_images(images,fake_img)
+            if data_type=='normal':
+                file_name = 'infer_normal_loss_' + str(loss.detach().cpu().numpy()) + "_" +  str(cnt) + '.jpg'
+            else:
+                file_name = 'infer_abnormal_loss_' + str(loss.detach().cpu().numpy()) + "_" + str(cnt) + '.jpg'
+            file_path = os.path.join('./runs/detect',file_name)
+            plts.savefig(file_path)
+            plts.show()
+            cnt+=1
+        show_num+=1
+    return loss_list
+
+def data_loader(shuffle,VAL_DATA_DIR,args):
+    size = (args.img_size,args.img_size)
+    img_test_data = torchvision.datasets.ImageFolder(VAL_DATA_DIR,
+                                                transform=transforms.Compose([
+                                                    transforms.Resize(size),
+                                                    #transforms.RandomHorizontalFlip(),
+                                                    #transforms.Scale(64),
+                                                    transforms.CenterCrop(size),
+                                
+                                                    transforms.ToTensor(),
+                                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), #GANomaly parameter
+                                                    ])
+                                                )
+
+    print('img_test_data length : {}'.format(len(img_test_data)))
+
+    test_loader = torch.utils.data.DataLoader(img_test_data, batch_size=args.batch_size,shuffle=shuffle,drop_last=True)
+    print('test_loader length : {}'.format(len(test_loader)))
+    
+    return test_loader
+
+
+        
+if __name__=="__main__":
+    
+    main()
---
+++ train.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 27 13:45:49 2022
+@author: User
+"""
+from six.moves import urllib
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+urllib.request.install_opener(opener)
+
+import torch
+from network import network
+import os
+import torchvision.transforms as transforms
+import torchvision
+import torch.nn as nn
+from util import loss
+from network.model import Ganomaly
+from network.umodel import UGanomaly
+from network.SAmodel import SAGanomaly
+from network.SSAmodel import SSAGanomaly
+from tqdm import tqdm
+from util import color
+from util.load_data import load_data, print_parameters
+
+
+
+def main():
+    args = get_args()
+    train(args)
+
+def train(args): 
+    print_parameters(args)
+    '''load data'''
+    train_loader = load_data(args)
+    test_loader = load_data(args,train=False,test=True)
+    '''load model'''
+    #skip_ganomaly = UGanomaly(args)
+    #ganomaly = Ganomaly(args)
+    #skip_attention_ganomaly = SAGanomaly(args)
+    Skip_SelfAttention_Ganomaly = SSAGanomaly(args)
+    #print(Skip_SelfAttention_Ganomaly)
+    ''' train epochs'''
+    train_epochs(Skip_SelfAttention_Ganomaly,train_loader,test_loader,args)
+
+
+def train_epochs(model,train_loader,test_loader,args):
+    ''' use gpu if available'''
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    _lowest_loss = 600.0
+    
+    _highest_auc = 0.0
+    
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+        
+    SAVE_MODEL_PATH = os.path.join(args.save_dir,"AE_3_best_2.pt")
+    SAVE_MODEL_G_PATH = os.path.join(args.save_dir,"netG.pt")
+    SAVE_MODEL_D_PATH = os.path.join(args.save_dir,"netD.pt")
+    
+    
+    SAVE_BEST_MODEL_G_PATH = os.path.join(args.save_dir,"best_netG.pt")
+    SAVE_BEST_MODEL_D_PATH = os.path.join(args.save_dir,"best_netD.pt")
+    
+    for epoch in range(1, args.epoch+1):
+        train_loss = 0.0
+        G_loss = 0
+        D_loss = 0
+        
+        
+        
+        pbar = tqdm(train_loader)
+        for images, _  in pbar: 
+            images = images.to(device)
+            '''inference'''
+            outputs = model(images)
+            error_g, error_d, fake_img, model_g, model_d, error_g_attn, error_g_ano_attn = outputs
+            loss = error_g + error_d
+            
+            bar_str = ' epoch:{} loss_g:{:.6f} loss_d:{:.6f} loss_g_attn:{:.6f} error_g_ano_attn:{:.6f}'.format(epoch,error_g.detach().cpu().numpy(),
+                                                                     error_d.detach().cpu().numpy(),
+                                                                     error_g_attn.detach().cpu().numpy(),
+                                                                     error_g_ano_attn.detach().cpu().numpy())
+            PREFIX = color.colorstr(bar_str)
+            pbar.desc = f'{PREFIX}'
+            ''' sum loss '''
+            G_loss += error_g.item()
+            D_loss += error_d.item()
+            
+            avg_g_loss = G_loss/len(train_loader)
+            avg_d_loss = D_loss/len(train_loader)
+        #train_loss = train_loss/len(train_loader)
+        
+        color_str = 'Epoch: {} \t Avg_G_loss: {:.6f} Avg_D_loss: {:.6f}'.format(epoch, avg_g_loss,avg_d_loss)
+        PREFIX = color.colorstr('green', 'bold',color_str)
+        print(PREFIX)
+        #print('Epoch: {} \t Avg_G_loss: {:.6f} Avg_D_loss: {:.6f}'.format(epoch, avg_g_loss,avg_d_loss))
+        
+        torch.save(model_g.state_dict(), SAVE_MODEL_G_PATH)
+        torch.save(model_d.state_dict(), SAVE_MODEL_D_PATH)
+        print('Save Epoch {} model complete !'.format(epoch))
+        #calculate auc
+        auc = model.test(test_loader)
+        print('auc = {:.6f}'.format(auc))
+        
+        if auc >= _highest_auc and epoch>1:
+            _highest_auc = auc
+            torch.save(model_g.state_dict(), SAVE_BEST_MODEL_G_PATH)
+            torch.save(model_d.state_dict(), SAVE_BEST_MODEL_D_PATH)
+            color_str = 'Save best-model weights complete with auc : {:.6f}'.format(_highest_auc)
+            PREFIX = color.colorstr('red', 'bold',color_str)
+            print(PREFIX)
+            #print('Save best-model weights complete with auc : %.6f'.format(_highest_auc))
+def compute_loss(outputs,images,criterion):
+    gen_imag, latent_i, latent_o = outputs
+    loss_con = loss.l2_loss(images, gen_imag)
+    loss_enc = loss.l1_loss(latent_i, latent_o)
+    loss_sum = loss_enc + 50*loss_con
+    return loss_sum
+
+
+def set_input(input:torch.Tensor):
+    """ Set input and ground truth
+    Args:
+        input (FloatTensor): Input data for batch i.
+    """
+    input = input.clone()
+    with torch.no_grad():
+        input.resize_(input[0].size()).copy_(input[0])
+       
+    return input
+
+
+def get_args():
+    import argparse
+    #isize=64, nz=100, nc=3
+    parser = argparse.ArgumentParser()
+    #'/home/ali/datasets/train_video/NewYork_train/train/images'
+    parser.add_argument('-imgdir','--img-dir',help='image dir',default=r"/home/ali/datasets/factory_data/2022-12-30-4cls-cropimg/images/train")
+    parser.add_argument('-imgtestdir','--img-testdir',help='val dataset',default=r"/home/ali/datasets/factory_data/2022-12-30-4cls-cropimg/images/val")
+    parser.add_argument('-imgsize','--img-size',type=int,help='image size',default=32)
+    parser.add_argument('-nz','--nz',type=int,help='compress length',default=100)
+    parser.add_argument('-nc','--nc',type=int,help='num of channel',default=3)
+    parser.add_argument('-lr','--lr',type=float,help='learning rate',default=2e-4)
+    parser.add_argument('-batchsize','--batch-size',type=int,help='train batch size',default=64)
+    parser.add_argument('-testbatchsize','--test_batchsize',type=int,help='test batch size',default=64)
+    parser.add_argument('-savedir','--save-dir',help='save model dir',default=r"/home/ali/GitHub_Code/cuteboyqq/GANomaly/skip-GANOMALY-Pytorch/runs/train/2023-01-13/32-nz100-ngf64-ndf64-Skip-SelfAttention-Ganomaly")
+    parser.add_argument('-weights','--weights',help='save model dir',default=r"")
+    parser.add_argument('-epoch','--epoch',type=int,help='num of epochs',default=60)
+    parser.add_argument('-train','--train',type=bool,help='train model',default=True)
+    return parser.parse_args()    
+
+if __name__=="__main__":
+    main()
